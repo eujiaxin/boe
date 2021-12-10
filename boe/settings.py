a9607deccc5546c1ce357bd08f40665ea588dbf1
heheheejin <e_jaxx@hotmail.com>
2021-12-11 06:46:21 +0800

    Create checker app for CSV uploads and outputs

--- boe/settings.py
+++ boe/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
-    'django_extensions'
+    'django_extensions',
+    'checkerapp.apps.CheckerappConfig'
 ]
 
 MIDDLEWARE = [
--- boe/urls.py
+++ boe/urls.py
@@ -39,5 +39,6 @@ router.register(r'users', user.UserViewSet, basename="user")
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('checkerapp/', include('checkerapp.urls'))
 ]
