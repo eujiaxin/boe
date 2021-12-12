55cdf271d3d86abfecb412163393d1cee6755c4b
Weiss01 <fabianlimxuhuai@gmail.com>
2021-12-12 11:19:29 +0800

    made sure file form only accepts csvs

--- checkerapp/views.py
+++ checkerapp/views.py
@@ -5,10 +5,11 @@ from django.views.generic.edit import FormView
 from api.models import CallistaDataFile
 from checkerapp.forms import CallistaDataFileMultipleUploadForm
 from django.http import HttpResponse
+from api.models import CallistaDataFile
 
 # Create your views here.
 
-# FIXME: make sure max 100 files upload -- override form validation, need to validate type = CSV?
+# FIXME: make sure max 100 files upload -- override form validation
 # TODO: already uploaded files to django. redirect to list of CSVs (checkbox page showing CSV processed or not processed yet) - need to process PUT request to handle csvs. this is where fabian comes in? (input = checked CSVs)
 
 
@@ -39,3 +40,30 @@ class CallistaDataFileCreateView(FormView):
 
 def success(request):
     return HttpResponse('<h1>Success!</h2>')
+
+
+# def processer(request):
+#     if request.method == "POST":
+#         files_to_delete = []
+#         for key, value in request.POST.items():
+#             if value == '1':
+#                 files_to_delete.append(key)
+#         print(files_to_delete)
+
+#         for result_file in ResultFile.objects.all():
+#             if result_file.file_name in files_to_delete:
+#                 print(f"Deleting {result_file.file_name} ...")
+#                 result_file.delete()
+
+#         for graph in Graph.objects.all():
+#             if graph.file_name in files_to_delete:
+#                 print(f"Deleting {graph.file_name} ...")
+#                 graph.delete()
+
+#     contexts = {
+#         'page_title': 'My Results',
+#         'visualiser_files': [
+#             f for f in ResultFile.objects.all().order_by('date_added')
+#         ],
+#     }
+#     return render(request, "plotter/mygraphs.html", context=contexts)
